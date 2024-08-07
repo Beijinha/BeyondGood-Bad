d4c21af67d4a3223fe4d91983715134ce7436b95
Marcelo Oliveira <beijinham@gmail.com>
2024-08-07 21:21:59 +0100

    Still trying

--- js/view/aboutView.js
+++ js/view/aboutView.js
@@ -1,6 +1,9 @@
 function render() {
+
   const container = document.querySelector('#container');
   container.innerHTML = ''; //removes the previous elements
+
+  window.location.href = "login.html";
 };
 
 export default { render };
